5736acdd24bee3efd073483415c91db8a8daa872
Luis A. Villalba <luisvillalba_12@hotmail.com>
2023-07-31 23:40:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -29,16 +29,6 @@
   // circle.bindPopup("¡Hola! Soy un círculo.");
   // polygon.bindPopup("¡Hola! Soy un polígono.");
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const map = new Cesium.Viewer("map", {
-        terrainProvider: Cesium.createWorldTerrain(),
-      });
-
-  const toggleTrajectoriesBtn = document.getElementById("toggleTrajectories");
-  const speedSlider = document.getElementById("speed");
-  const styleSelect = document.getElementById("style");
-    };
-
 const PlatziSat_01 = 
   `1 88888U 24001FA  23163.94096086  .00000000  00000-0  10000-4 0  9999
   2 88888  97.5077 280.5424 0008220 228.6198 130.8530 15.11803180  1009`;
